9005e46ffe489461e93b8abdd91eb8231ca48cb1
AgustinaT <agustina.tornari@gmail.com>
2022-06-27 20:27:04 -0300

    contador con boton + Tornari

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './ItemCount.css';
+
+const ItemCount = ({ onAdd }) => {
+    const initial = 1;
+    const stock = 7; 
+  
+    //hook de estado
+    const [contador, setContador] = useState(initial);
+  
+    const addProduct = (num) => {
+        setContador(contador + num);
+    };
+  
+    return (
+      <div className="count-container">
+        <div className="count-container-contador">
+          <button
+            className="count-container-button"
+            onClick={() => addProduct(-1)}
+            disabled={contador === initial ? true : null}
+          > - </button>
+
+          <span className="count-container__qty">{contador}</span>
+          <button
+            className="count-container-button"
+            onClick={() => addProduct(+1)}
+            disabled={contador === stock ? true : null}
+          > + </button>
+        </div>
+  
+        <button
+          className="button-primary"
+          onClick={() => onAdd(contador)}
+          disabled={stock === 0 ? true : null}
+        >Añadir al carrito</button>
+      </div>
+    );
+  };
+  
+  export default ItemCount;
+  
--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ItemCount from '../../components/ItemCount/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (contador) => {
+    console.log("Se agrego al carrito")
+  }
+
   return (
     <div>
       <p>{greeting}</p>
+      <ItemCount onAdd={onAdd}/>
     </div>
   )
 }
